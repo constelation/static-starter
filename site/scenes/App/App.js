46b795a8a901b6fd7b55c277e310e1c5ea32cf1c
Kyle Poole <2kylepoole@gmail.com>
2016-08-09 08:11:16 -0700

    update eslint

--- site/scenes/App/App.js
+++ site/scenes/App/App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
           userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36',
         }}
       >
-        <Style rules={normalize}/>
+        <Style rules={normalize} />
         {this.props.children}
       </StyleRoot>
     )
