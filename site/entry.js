9285879c0c4a0b4a7f62aadc4980b9adb23640fe
Kyle Poole <2kylepoole@gmail.com>
2016-12-21 15:57:23 -0800

    update head's meta tags to follow https://github.com/joshbuchea/HEAD

--- site/entry.js
+++ site/entry.js
@@ -61,11 +61,13 @@ export default (locals: Object, callback: Function) => {
       <!DOCTYPE html>
       <html>
         <head>
+          <meta charset="utf-8">
+          <meta http-equiv="x-ua-compatible" content="ie=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <!-- The above 3 meta tags *must* come first in the head, according to https://github.com/joshbuchea/HEAD; any other head content must come *after* these tags -->
+          <title>Site Title</title>
           <style>${globalStyles}</style>
           <style>${css}</style>
-          <title>Site Title</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-          <meta http-equiv="content-type" content="text/html; charset=utf-8">
         </head>
         <body>
           <div id="root">
