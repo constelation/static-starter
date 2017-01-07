7d256a6cc398176a7a6d4f956009248d81e5894b
Kyle Poole <2kylepoole@gmail.com>
2017-01-06 18:13:32 -0800

    update importjs

--- .importjs.js
+++ .importjs.js
@@ -1,15 +1,21 @@
 module.exports = {
   aliases: {
     React: 'react',
-    Text: 'constelation-Text',
+    mobx: 'mobx',
+    // constelation
+    Video: 'constelation-Video',
+    TransitionGroupView: 'constelation-transition-group-view',
     View: 'constelation-View',
     Col: 'constelation-Col',
     Row: 'constelation-Row',
+    Flex: 'constelation-Flex',
     Style_: 'constelation-Style_',
-    Animate_: 'constelation-Animate_',
     Event_: 'constelation-Event_',
+    Animate_: 'constelation-Animate_',
+    keydown: 'constelation-keydown-decorator',
   },
   groupImports: true,
+  useRelativePaths: false,
   importStatementFormatter({ importStatement }) {
     return importStatement.replace(/;$/, '');
   }
