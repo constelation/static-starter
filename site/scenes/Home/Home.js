273e6524372f7bde3780fcf65b6a5ac682f9e843
Kyle Poole <2kylepoole@gmail.com>
2017-01-06 17:06:11 -0800

    update constelation libs

--- site/scenes/Home/Home.js
+++ site/scenes/Home/Home.js
@@ -13,7 +13,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <Style_ backgroundColor='red'>
-        <View alignVertical='center' alignHorizontal='center' height={100} >
+        <View center height={100} >
           <Text size={20}>Home</Text>
           <Link to='other'>Other</Link>
         </View>
