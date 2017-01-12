6190e3ec2fb41a48f0debf11e14381b016369744
Kyle Poole <2kylepoole@gmail.com>
2017-01-12 11:49:28 -0800

    HMR works, but requires page refresh

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,20 @@ const paths = [
 module.exports = function (env = {}) {
   const config = {
     entry: {
-      'main': './site/entry.js',
+      'main': env.buildSite ? './site/entry.js' : [
+        'react-hot-loader/patch',
+        // activate HMR for React
+
+        'webpack-dev-server/client?http://localhost:8080',
+        // bundle the client for webpack-dev-server
+        // and connect to the provided endpoint
+
+        'webpack/hot/only-dev-server',
+        // bundle the client for hot reloading
+        // only- means to only hot reload for successful updates
+
+        './site/entry.js'
+      ],
     },
 
     output: {
@@ -24,7 +37,7 @@ module.exports = function (env = {}) {
       // filename: '[chunkhash].[name].js',
 
       // Put generated files in this directory
-      path: 'public',
+      path: path.join(__dirname, './public'),
 
       // When a loader does not inline, prefix their urls with publicPath
       publicPath: '/',
@@ -64,28 +77,18 @@ module.exports = function (env = {}) {
       ],
     },
 
+    devtool: 'inline-source-map',
+
     devServer: {
+      hot: true,
+      // enable HMR on the server
+
       // webpack-dev-server uses generated index.html from `npm run build`
       contentBase: 'public',
     },
   }
 
   config.plugins = [
-
-    // Automatically loaded modules - means these imports are not needed in each file
-    new webpack.ProvidePlugin({
-      React: 'react',
-      Col: 'constelation-Col',
-      Button: 'constelation-Button',
-      BackgroundImage: 'constelation-BackgroundImage',
-      Row: 'constelation-Row',
-      Flex: 'constelation-Flex',
-      Text: 'constelation-Text',
-      View: 'constelation-View',
-      Style_: 'constelation-Style_',
-      Event_: 'constelation-Event_',
-      Animate_: 'constelation-Animate_',
-    }),
 
     new webpack.DefinePlugin({
       __DEV__: env.dev || false,
@@ -125,6 +128,14 @@ module.exports = function (env = {}) {
   }
 
   if (env.dev) {
+    config.plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+      // enable HMR globally
+
+      new webpack.NamedModulesPlugin()
+      // prints more readable module names in the browser console on HMR updates
+    )
+
     // disable performance warnings in dev mode
     // it'll always be too big
     config.performance = {
