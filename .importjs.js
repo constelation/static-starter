5e7a07e4349c63cbc1f3be67551d0d906da79a1d
Kyle Poole <2kylepoole@gmail.com>
2017-02-28 15:52:17 -0800

    updated .importjs.js

--- .importjs.js
+++ .importjs.js
@@ -3,20 +3,37 @@ module.exports = {
     React: 'react',
     mobx: 'mobx',
     // constelation
+    Text: 'constelation-text',
     Video: 'constelation-Video',
     TransitionGroupView: 'constelation-transition-group-view',
+    ScrollView: 'constelation-scroll-view',
     View: 'constelation-view',
-    Col: 'constelation-Col',
-    Row: 'constelation-Row',
-    Flex: 'constelation-Flex',
     Style_: 'constelation-style_',
-    Event_: 'constelation-Event_',
-    Animate_: 'constelation-Animate_',
+    Event_: 'constelation-event_',
+    Animate_: 'constelation-animate_',
     keydown: 'constelation-keydown-decorator',
+    // shared
+    // TODO: fill in the rest
   },
   groupImports: true,
-  useRelativePaths: false,
+  useRelativePaths({ pathToImportedModule, pathToCurrentFile }) {
+    // relative iff there is an underscore (subcomponent) in path
+    if (/_/.test(pathToImportedModule)) {
+      return true;
+    }
+    return false;
+  },
   importStatementFormatter({ importStatement }) {
+    // no semicolons
     return importStatement.replace(/;$/, '');
+  },
+  moduleNameFormatter({ moduleName, pathToCurrentFile }) {
+    // shortcut for aliased paths
+    if (moduleName.startsWith('site/')) {
+      return moduleName.slice(5);
+    }
+    // Fall back to the original specifier. It's important that this function
+    // always returns a string.
+    return moduleName;
   }
 }
